f00ef4f1fb081d73ba3f6642cdcc98776411ab42
geekwise-deidra-wallert <geekwise.deidra.wallert@gmail.com>
2019-11-25 23:08:25 +0000

    created models need to work on connecting to products

--- app/bank/admin.py
+++ app/bank/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Branch, Client, Product, Account
 
-# Register your models here.
+admin.site.register(Branch)
+admin.site.register(Client)
+admin.site.register(Account)
+admin.site.register(Product)
+
